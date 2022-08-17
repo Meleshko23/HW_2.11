80b5bea42a976789cdc5201af0d142cc4da9e4eb
lenovo <dgon1976@mail.ru>
2022-08-17 19:19:31 +0500

    ДЗ 2.11 выполнено

---
+++ src/main/java/com/example/hw_2_11/service/StoreService.java
@@ -0,0 +1,28 @@
+package com.example.hw_2_11.service;
+
+import com.example.hw_2_11.component.Basket;
+import com.example.hw_2_11.model.Item;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StoreService {
+
+    private final Basket basket;
+
+    public StoreService(Basket basket) {
+        this.basket = basket;
+    }
+
+    public Item add(long id) {
+        Item item = new Item(id);
+        basket.add(item);
+        return item;
+    }
+
+    public List<Item> getItems() {
+        return basket.getItems();
+    }
+}
